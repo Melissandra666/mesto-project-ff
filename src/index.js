d1b0a37cda3cc98cd88452ef530cd584dc67f2a0
Melissandra666 <157244568+Melissandra666@users.noreply.github.com>
2024-07-16 00:41:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import addIcon from './images/add-icon.svg';
-import avatar from './images/avatar.jpg';
-import cardOne from './images/card_1.jpg';
-import cardTwo from './images/card_2.jpg';
-import cardThree from './images/card_3.jpg';
-import close from './images/close.svg';
-import deleteIcon from './images/delete-icon.svg';
-import editIcon from './images/edit-icon.svg';
-import likeActive from './images/like-active.svg';
-import likeInactive from './images/like-inactive.svg';
-import logo from './images/logo.svg';
 import './pages/index.css';
 import { initialCards } from './components/cards.js';
 import { createCard, handleLikeClick, handleDeleteClick} from './components/card.js';
