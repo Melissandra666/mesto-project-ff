55186e52e179a2f343bd7dbd79bb5f8c07591b9c
Melissandra666 <157244568+Melissandra666@users.noreply.github.com>
2024-08-10 00:51:35 +0300

    Update validation.js

--- src/components/validation.js
+++ src/components/validation.js
@@ -1,60 +1,61 @@
 function checkInputValidity(inputElement, config) {
-  const errorMessage = '';
+  let errorMessage = ""; 
   if (inputElement.validity.valueMissing) {
-    errorMessage = 'Вы пропустили это поле.';
-  }
-  else if (inputElement.validity.tooShort) {
+    errorMessage = "Вы пропустили это поле.";
+  } else if (inputElement.validity.tooShort) {
     errorMessage = `Должно быть от ${inputElement.minLength} до ${inputElement.maxLength} символов.`;
-  }
-  else if (inputElement.validity.patternMismatch) {
-    errorMessage = inputElement.dataset.errorMessage || 'Неверный формат.';
-  }
-  else if (inputElement.validity.typeMismatch) {
-    if (inputElement.type === 'url') {
-      errorMessage = 'Введите адрес сайта.';
+  } else if (inputElement.validity.patternMismatch) {
+    errorMessage = inputElement.dataset.errorMessage || "Неверный формат.";
+  } else if (inputElement.validity.typeMismatch) {
+    if (inputElement.type === "url") {
+      errorMessage = "Введите адрес сайта.";
     } else {
-      errorMessage = 'Введите корректное значение.';
+      errorMessage = "Введите корректное значение.";
     }
-  };
+  }
   if (!inputElement.validity.valid) {
-    showInputError(inputElement, errorMessage, config);
-  } else {
-    hideInputError(inputElement, config)
-  };
-  inputElement.addEventListener('input', checkInputValidity);
+    showError(inputElement, errorMessage, config);
+    } else {
+      hideError(inputElement, config)
+    };
+   inputElement.addEventListener('input', checkInputValidity);
 };
 
-function hideInputError(inputElement, config) {
-  const errorElement = inputElement.closest(config.formSelector).querySelector(`.${inputElement.name}-input-error`);
+function hideError(inputElement, config) {
+  const errorElement = inputElement
+    .closest(config.formSelector)
+    .querySelector(`.${inputElement.name}-input-error`);
   if (errorElement) {
-    errorElement.textContent = ''; 
+    errorElement.textContent = "";
     errorElement.classList.remove(config.errorClass);
-    inputElement.classList.remove(config.inputErrorClass); 
+    inputElement.classList.remove(config.inputErrorClass);
   }
 };
 
-function showInputError(inputElement, errorMessage, config) {
-  const errorElement = inputElement.closest(config.formSelector).querySelector(`.${inputElement.name}-input-error`);
+function showError(inputElement, errorMessage, config) {
+  const errorElement = inputElement
+    .closest(config.formSelector) 
+    .querySelector(`.${inputElement.name}-input-error`);
   if (errorElement) {
-    errorElement.textContent = errorMessage;
+    errorElement.textContent = errorMessage; 
     errorElement.classList.add(config.errorClass);
-    inputElement.classList.add(config.inputErrorClass);
+    inputElement.classList.add(config.inputErrorClass); 
   }
 };
 
 function hasInvalidInput(inputList) {
-  return inputList.some((inputElement) => !inputElement.validity.valid); 
+  return inputList.some((inputElement) => !inputElement.validity.valid);
 };
 
 function toggleButtonState(inputList, buttonElement, config) {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add(config.inactiveButtonClass);
-    buttonElement.disabled = true;
+    buttonElement.classList.add(config.inactiveButtonClass); 
+    buttonElement.disabled = true; 
   } else {
-    buttonElement.classList.remove(config.inactiveButtonClass);
+    buttonElement.classList.remove(config.inactiveButtonClass); 
     buttonElement.disabled = false;
   }
-};
+}
 
 function enableValidation(config) {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
@@ -62,12 +63,12 @@ function enableValidation(config) {
     const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
     const buttonElement = formElement.querySelector(config.submitButtonSelector);
     inputList.forEach((inputElement) => {
-      inputElement.addEventListener('input', () => {
-        checkInputValidity(inputElement, config);
-        toggleButtonState(inputList, buttonElement, config);
+      inputElement.addEventListener("input", () => {
+        checkInputValidity(inputElement, config); 
+        toggleButtonState(inputList, buttonElement, config); 
       });
     });
-    formElement.addEventListener('submit', (event) => event.preventDefault());
+    formElement.addEventListener("submit", (event) => event.preventDefault());
     toggleButtonState(inputList, buttonElement, config);
   });
 };
@@ -75,9 +76,9 @@ function enableValidation(config) {
 function clearValidation(formElement, config) {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
   const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  inputList.forEach((inputElement) => hideInputError(inputElement, config));
+  inputList.forEach((inputElement) => hideError(inputElement, config));
   buttonElement.classList.add(config.inactiveButtonClass);
-  buttonElement.disabled = true;
-};
+  uttonElement.disabled = true;
+}
 
 export { enableValidation, clearValidation };
